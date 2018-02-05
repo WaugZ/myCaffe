5842714ffbf62d2f1891b00435722bb5c103b0fb
wangzi <wangzi@git.imagedt.com>
2018-02-05 14:44:55 +0800

    优化lmdb生成方式，减少内存消耗

--- dev/create_lmdb.py
+++ dev/create_lmdb.py
@@ -73,26 +73,29 @@ def create_lmdb(train_txt_path, train_lmdb_output_path, val_txt_path="", val_lmd
 
     # 打开 lmdb 环境, 生成一个数据文件，定义最大空间, 1e12 = 1000000000000.0
     in_db = lmdb.open(train_lmdb, map_size=int(1e12))
-    with in_db.begin(write=True) as in_txn: # 创建操作数据库句柄
-        for in_idx, img_label in enumerate(train_data):
-            # 读取图像. 做直方图均衡化、裁剪操作
-            img_path = img_label[0]
-            img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-            img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
-            label = int(img_label[1])
-            # if 'cat' in img_path: # 组织 label, 这里是如果文件名称中有 'cat', 标签就是 0
-            #     label = 0         # 如果图像名称中没有 'cat', 有的是 'dog', 标签则为 1
-            # else:                 # 这里方, label 需要自己去组织
-            #     label = 1         # 每次情况可能不一样, 灵活点
-            # img = img[:, :, ::-1]
-            # img = img.transpose((2, 0, 1))
-            datum = make_datum(img, label)
-            # '{:0>5d}'.format(in_idx):
-            #      lmdb的每一个数据都是由键值对构成的,
-            #      因此生成一个用递增顺序排列的定长唯一的key
-            in_txn.put('{:0>5d}'.format(in_idx), datum.SerializeToString()) # 调用句柄，写入内存
-            if in_idx % 1000 == 0:
-                print "processed {:d} train pictures".format(in_idx)
+    in_txn = in_db.begin(write=True)  # 创建操作数据库句柄
+    for in_idx, img_label in enumerate(train_data):
+        # 读取图像. 做直方图均衡化、裁剪操作
+        img_path = img_label[0]
+        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+        img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
+        label = int(img_label[1])
+        # if 'cat' in img_path: # 组织 label, 这里是如果文件名称中有 'cat', 标签就是 0
+        #     label = 0         # 如果图像名称中没有 'cat', 有的是 'dog', 标签则为 1
+        # else:                 # 这里方, label 需要自己去组织
+        #     label = 1         # 每次情况可能不一样, 灵活点
+        # img = img[:, :, ::-1]
+        # img = img.transpose((2, 0, 1))
+        datum = make_datum(img, label)
+        # '{:0>5d}'.format(in_idx):
+        #      lmdb的每一个数据都是由键值对构成的,
+        #      因此生成一个用递增顺序排列的定长唯一的key
+        in_txn.put('{:0>8d}'.format(in_idx), datum.SerializeToString())  # 调用句柄，写入内存
+        if in_idx % 1000 == 0:
+            in_txn.commit()
+            in_txn = in_db.begin(write=True)
+            print "processed {:d} train pictures".format(in_idx)
+    print "Finish processing all {:d} training pictures".format(in_idx + 1)
 
     # 结束后记住释放资源，否则下次用的时候打不开。。。
     in_db.close()
@@ -101,25 +104,26 @@ def create_lmdb(train_txt_path, train_lmdb_output_path, val_txt_path="", val_lmd
         # 创建验证集 lmdb 格式文件
         print '\nCreating validation_lmdb'
         in_db = lmdb.open(val_lmdb, map_size=int(1e12))
-        with in_db.begin(write=True) as in_txn:
-            for in_idx, img_label in enumerate(test_data):
-                img_path = img_label[0]
-                img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-                img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
-                label = int(img_label[1])
-                # if 'cat' in img_path:
-                #     label = 0
-                # else:
-                #     label = 1
-                # img = img[:, :, ::-1]
-                # img = img.transpose((2, 0, 1))
-                datum = make_datum(img, label)
-                in_txn.put('{:0>5d}'.format(in_idx), datum.SerializeToString())
-                if in_idx % 1000 == 0:
-                    print "processed {:d} val pictures".format(in_idx)
+        in_txn = in_db.begin(write=True)
+        for in_idx, img_label in enumerate(test_data):
+            img_path = img_label[0]
+            img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+            img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
+            label = int(img_label[1])
+            # if 'cat' in img_path:
+            #     label = 0
+            # else:
+            #     label = 1
+            # img = img[:, :, ::-1]
+            # img = img.transpose((2, 0, 1))
+            datum = make_datum(img, label)
+            in_txn.put('{:0>8d}'.format(in_idx), datum.SerializeToString())
+            if in_idx % 1000 == 0:
+                in_txn.commit()
+                in_txn = in_db.begin(write=True)
+                print "processed {:d} val pictures".format(in_idx)
         in_db.close()
-
-    print '\nFinished processing all images'
+    print "Finish processing all {:d} val pictures".format(in_idx + 1)
 
 
 if __name__ == "__main__":
--- dev/train_restore.py
+++ dev/train_restore.py
@@ -13,25 +13,30 @@ caffe.set_device(0)
 
 solver = caffe.SGDSolver(solver_path)
 
-solverstates = [ss for ss in os.listdir(source_path) if ss.endswith(".solverstate")]
-if not len(solverstates) == 0:
-    latest_solver = ""
-    latest_iter = 0
-    for solverstate in solverstates:
-        pattern = r"\d+"
-        reg = re.findall(pattern, solverstate)
-        i = int(reg[0])
-        if i > latest_iter:
-            latest_iter = i
-            latest_solver = solverstate
+RESTORE = True
 
-    latest_solver = os.path.join(source_path, latest_solver)
-    # print latest_solver
+if RESTORE:
+    solverstates = [ss for ss in os.listdir(source_path) if ss.endswith(".solverstate")]
+    if len(solverstates) > 0:
+        latest_solver = ""
+        latest_iter = 0
+        for solverstate in solverstates:
+            pattern = r"\d+"
+            reg = re.findall(pattern, solverstate)
+            iter = int(reg[0])
+            if iter > latest_iter:
+                latest_iter = iter
+                latest_solver = solverstate
 
-    solver.restore(latest_solver)
+        latest_solver = os.path.join(source_path, latest_solver)
+        # print latest_solver
 
-if os.path.isfile(solverstate_path) and solverstate_path.endswith(".solverstate"):
-    solver.restore(solverstate_path)
+        solver.restore(latest_solver)
+
+    elif os.path.isfile(solverstate_path) and solverstate_path.endswith(".solverstate"):
+        solver.restore(solverstate_path)
+    else:
+        print "can not solver restore from solverstate, the solver will solve from the beginning."
 
 while solver.iter < solver.param.max_iter:
     solver.step(1)
